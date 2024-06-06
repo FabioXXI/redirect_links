439f49b4e5fbc07f753281dea6d1d995a208d818
FabioXXI <astore.a.fabio@gmail.com>
2024-06-06 16:16:39 -0300

    stable

---
+++ redirect.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+
+app = FastAPI()
+
+URLS = {
+    "google": "https://google.com"
+}
+
+@app.get("")
+async def redirect():
+    return RedirectResponse(URLS["google"])
